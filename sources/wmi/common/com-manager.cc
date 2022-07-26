2f92ef4f1bd47fa67e88345b5c961f84fd049d8e
ali bozna <alibozna@gmail.com>
2022-07-26 20:50:04 +0200

    Add ComManager

---
+++ sources/wmi/common/com-manager.cc
@@ -0,0 +1,33 @@
+#include <wmi/common/com-manager.h>
+#include <wmi/common/com-exception.h>
+
+#include <Windows.h>
+
+
+using namespace wmi;
+
+
+void ComManager::Initialize(std::optional<ProcessSecurity> security)
+{
+  auto security_settings = security.has_value() ? security.value() : ProcessSecurity();
+
+  ComExceptionFactory::ThrowIfFailed(CoInitializeEx(nullptr, COINIT_MULTITHREADED));
+
+  auto result = CoInitializeSecurity(nullptr,
+                                     -1,
+                                     nullptr,
+                                     nullptr,
+                                     static_cast<DWORD>(security_settings.authentication),
+                                     static_cast<DWORD>(security_settings.impersonation),
+                                     nullptr,
+                                     EOAC_NONE,
+                                     nullptr);
+
+  ComExceptionFactory::ThrowIfFailed(result);
+}
+
+
+void ComManager::Unitialize()
+{
+  CoUninitialize();
+}
---
+++ sources/wmi/common/com-manager.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <wmi/common/exports.h>
+
+#include <optional>
+
+
+namespace wmi {
+
+enum class ImpersonationLevel
+{
+	kDefault,
+	kAnonymouse,
+	kIdentify,
+	kImpersonate,
+	kDelegate
+};
+
+
+enum class AuthenticationLevel
+{
+	kDefault,
+	kNone,
+	kConnect,
+	kCall,
+	kPacket,
+	kPacketIntegrity,
+	kPacketPrivacy
+};
+
+
+struct ProcessSecurity
+{
+	ImpersonationLevel impersonation = ImpersonationLevel::kImpersonate;
+	AuthenticationLevel authentication = AuthenticationLevel::kDefault;
+};
+
+
+class WMI_DLL ComManager
+{
+public:
+  static void Initialize(std::optional<ProcessSecurity> security = std::nullopt);
+	static void Unitialize();
+};
+
+}
