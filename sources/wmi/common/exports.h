b79322e4a4ac236f73e179671270ab25f4f77645
bozali <alibozna@gmail.com>
2024-10-30 19:11:39 +0100

    Project setup

---
+++ sources/wmi/common/exports.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#if defined(WMI_EXPORTS)
+#	define WMI_DLL __declspec(dllexport)
+#else
+#	define WMI_DLL __declspec(dllimport)
+#endif // WMI_EXPORTS
+
+
+#define WMI_EXTERN_C extern "C"
